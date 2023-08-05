b9acfa7e8d8f08088ee2bd85caefd6c99edd93e9
Tymora <dachabel@mail.ru>
2023-08-05 16:35:02 +0300

    Вторая стадия

--- task/src/chucknorris/Main.kt
+++ task/src/chucknorris/Main.kt
@@ -2,7 +2,9 @@ package chucknorris
 
 fun main() {
     println("Input string:")
-    for (i in readln()){
-        print("$i ")
+    val x = readln()
+    println("The result:")
+    for (i in x) {
+        println("$i = ${i.code.toString(2).padStart(7, '0')}")
     }
 }
